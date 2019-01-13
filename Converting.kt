8c4bc7aee2f1775b026845248bf49184b5dd25b5
EdManDev <ed.manigat@gmail.com>
2019-01-13 12:21:13 -0400

    add

---
+++ Converting.kt
@@ -0,0 +1,27 @@
+package com.example.edman.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+------------------------------------------------------
+
+package com.example.edman.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
